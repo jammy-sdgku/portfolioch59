f83c17929ace043f7f5ef205858e55d553a9c07c
jammy-sdgku <james.jones@sdgku.edu>
2025-11-18 18:23:31 +0000

    Add post-deploy hook to fix static file permissions

---
+++ .platform/hooks/postdeploy/01_fix_static_permissions.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Fixing static files permissions..."
+chmod -R 755 /var/app/current/staticfiles/
+chown -R webapp:webapp /var/app/current/staticfiles/
+echo "Static files permissions fixed!"
