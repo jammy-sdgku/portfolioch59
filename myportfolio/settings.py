90c62986d0730fb0dc902adf578b8be58903095e
jammy-sdgku <james.jones@sdgku.edu>
2025-11-18 02:19:50 +0000

    Fix S3 region configuration for media files

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -151,10 +151,11 @@ if not DEBUG:
     AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
     AWS_STORAGE_BUCKET_NAME = 'elasticbeanstalk-eu-north-1-603319527322'
-    AWS_S3_REGION_NAME = 'eu-north-1'
+    AWS_S3_REGION_NAME = 'eu-north-1'  # Fix: Ensure this is set correctly
     AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.{AWS_S3_REGION_NAME}.amazonaws.com'
     AWS_DEFAULT_ACL = 'public-read'
     AWS_LOCATION = 'portfolio-media'
+    AWS_S3_SIGNATURE_VERSION = 's3v4'  # Add this line
     
     # S3 Media Storage
     DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
