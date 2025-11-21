ab237b4a74255162b7f9162974ed9bd1e6d4dfee
jammy-sdgku <james.jones@sdgku.edu>
2025-11-21 19:07:45 +0000

    Fix experience view to remove certifications from context

--- experience/views.py
+++ experience/views.py
@@ -3,10 +3,8 @@ from .models import Experience, Education, Certification
 
 def experience_view(request):
     experiences = Experience.objects.prefetch_related('skills').all()
-    certifications = Certification.objects.prefetch_related('skills').all()
     context = {
         'experiences': experiences,
-        'certifications': certifications,
     }
     return render(request, 'portfolio/experience.html', context)
 
